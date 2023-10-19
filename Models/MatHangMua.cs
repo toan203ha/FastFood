e23426e79b76919194ea8edceb6b629b6df266cf
Pontax <toan30112003@gmail.com>
2023-10-19 21:19:42 +0700

    Thông điệp commit

---
+++ Models/MatHangMua.cs
@@ -0,0 +1,11 @@
+﻿namespace CNPM_NC_DoAnNhanh.Models
+{
+    public class MatHangMua
+    {
+        public string TenMonAn { get; set; } = "";
+        public string ád { get; set; } = "";
+        public string TenMoádanAn { get; set; } = "";
+        public string TenMonádaAn { get; set; } = "";
+
+    }
+}
